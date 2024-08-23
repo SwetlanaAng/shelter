985c13768141a27dc6e284fd6bc8615be2a44e28
Svetlana Angeliuk <floss2@yandex.ru>
2024-08-23 02:37:11 +0200

    feat: add slider main 1280

--- shelter/index.js
+++ shelter/index.js
@@ -1,9 +1,110 @@
+let arrPets = [
+    {
+        "name": "Katrine",
+        "img": "./img/katrine.png",
+        "type": "Cat",
+        "breed": "British Shorthair",
+        "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
+        "age": "6 months",
+        "inoculations": ["panleukopenia"],
+        "diseases": ["none"],
+        "parasites": ["none"]
+      },
+    {
+      "name": "Jennifer",
+      "img": "./img/jennifer.png",
+      "type": "Dog",
+      "breed": "Labrador",
+      "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+      "age": "2 months",
+      "inoculations": ["none"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Sophia",
+      "img": "./img/sophia.png",
+      "type": "Dog",
+      "breed": "Shih tzu",
+      "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+      "age": "1 month",
+      "inoculations": ["parvovirus"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Woody",
+      "img": "./img/woody.png",
+      "type": "Dog",
+      "breed": "Golden Retriever",
+      "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
+      "age": "3 years 6 months",
+      "inoculations": ["adenovirus", "distemper"],
+      "diseases": ["right back leg mobility reduced"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Scarlett",
+      "img": "./img/scarlet.png",
+      "type": "Dog",
+      "breed": "Jack Russell Terrier",
+      "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
+      "age": "3 months",
+      "inoculations": ["parainfluenza"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    
+    {
+      "name": "Timmy",
+      "img": "./img/timmy.png",
+      "type": "Cat",
+      "breed": "British Shorthair",
+      "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
+      "age": "2 years 3 months",
+      "inoculations": ["calicivirus", "viral rhinotracheitis"],
+      "diseases": ["kidney stones"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Freddie",
+      "img": "./img/freddie.png",
+      "type": "Cat",
+      "breed": "British Shorthair",
+      "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
+      "age": "2 months",
+      "inoculations": ["rabies"],
+      "diseases": ["none"],
+      "parasites": ["none"]
+    },
+    {
+      "name": "Charly",
+      "img": "./img/charly.png",
+      "type": "Dog",
+      "breed": "Jack Russell Terrier",
+      "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
+      "age": "8 years",
+      "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
+      "diseases": ["deafness", "blindness"],
+      "parasites": ["lice", "fleas"]
+    },
+  ]
 document.addEventListener("DOMContentLoaded", function(){
     const burger = document.querySelector(".nav");
     
     const menu = document.querySelector(".menu");
     const menuBackground = document.querySelector(".menu_background");
     menuBackground.classList.add('hide');
+    const cardsWrapper = document.querySelector(".cards_wrapper"); //лента
+    const cardsWindow = document.querySelector(".cards_window"); // окошко
+    const width = window.getComputedStyle(cardsWindow).width;
+    const arrowNext = document.querySelector(".arrow_next>.arrow");
+    const arrowPrev = document.querySelector(".arrow_prev>.arrow");
+
+    let arrShowCards = [];
+    let arrPetsInWrapper = [];
+
+    // burger
 
     function openCloseBurger(){
         menuBackground.classList.toggle('hide');
@@ -19,6 +120,87 @@ document.addEventListener("DOMContentLoaded", function(){
             openCloseBurger();
         }
     })
+
+    // slider main
+
+    console.log(document.documentElement.clientWidth);
+
+    arrPets = arrPets.sort(() => Math.random() - 0.5);
+    arrShowCards = [arrPets[0], arrPets[1], arrPets[2]]; 
+    arrPetsInWrapper = [...genCards(arrPets, arrShowCards), ...arrShowCards, ...genCards(arrPets, arrShowCards)];
+    putCardInEnd(0, arrPetsInWrapper.length);
+
+    function createCard(index) {
+        const cardSource = arrPetsInWrapper[index];
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+            <div class="card_img"><img src="${cardSource.img}" alt="${cardSource.name}"></div>
+                        <h4>${cardSource.name}</h4>
+                        <button class="light_button">Learn more</button>`
+            return card;
+    }
+    function putCardInEnd(from, to){
+        for(let i = from; i<to; i++){
+            cardsWrapper.append(createCard(i));
+        } 
+    }
+
+    function putCardInBeginning(){
+        for(let i = 0; i<3; i++){
+            cardsWrapper.prepend(createCard(i));
+        }
+        
+    }
+
+    function genCards(arrInitial, arrMiddleCards){
+        let genedArr = []
+        let i = 0;
+        arrInitial = arrInitial.sort(() => Math.random() - 0.5);
+        while (genedArr.length<3){
+            if(arrInitial[i]!=arrMiddleCards[0] && arrInitial[i]!=arrMiddleCards[1] && arrInitial[i]!=arrMiddleCards[2]){
+                genedArr.push(arrInitial[i]);
+                
+            }
+            i++;
+        }
+        console.log(arrMiddleCards[0].name, arrMiddleCards[1].name, arrMiddleCards[2].name, genedArr[0].name, genedArr[1].name, genedArr[2].name);
+        return genedArr;
+      }
+
+      arrowNext.addEventListener('click', ()=>{
+        cardsWrapper.style.transitionDuration = '0s';
+        cardsWrapper.children[0].remove();
+        cardsWrapper.children[0].remove();
+        cardsWrapper.children[0].remove();
+        cardsWrapper.style.left = '0px';
+        arrShowCards = [arrPetsInWrapper[6], arrPetsInWrapper[7], arrPetsInWrapper[8]];
+        arrPetsInWrapper.push(...genCards(arrPets, arrShowCards));
+        arrPetsInWrapper.splice(0, 3);
+        putCardInEnd(6, 9);
+
+        setTimeout(()=>{
+            cardsWrapper.style.transitionDuration = '0.5s';
+            cardsWrapper.style.left = '-1080px';
+        }, 0);
+        
+      })
+
+      arrowPrev.addEventListener('click', ()=>{
+        cardsWrapper.style.transitionDuration = '0s';
+        cardsWrapper.children[cardsWrapper.children.length-1].remove();
+        cardsWrapper.children[cardsWrapper.children.length-1].remove();
+        cardsWrapper.children[cardsWrapper.children.length-1].remove();
+        arrShowCards = [arrPetsInWrapper[0], arrPetsInWrapper[1], arrPetsInWrapper[2]];
+        arrPetsInWrapper.unshift(...genCards(arrPets, arrShowCards));
+        arrPetsInWrapper.splice(6, 3);
+        putCardInBeginning();
+        cardsWrapper.style.left = '-2160px';
+        setTimeout(()=>{
+            cardsWrapper.style.transitionDuration = '0.5s';
+            cardsWrapper.style.left = '-1080px';
+        }, 0);
+      })
 })
 
 
