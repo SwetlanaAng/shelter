4fc5142ce0b9fcc267f12f52badbc7e66a1bf892
Svetlana Angeliuk <floss2@yandex.ru>
2024-09-18 22:05:22 +0200

    feat: add html and css

---
+++ audio-player/index.js
@@ -0,0 +1,33 @@
+'use strict';
+const songsData = [
+    {
+        "src": "./img/lemonade.png",
+        "alt": "singer photo",
+        "author": "Beyonce",
+        "song": "Don't hurt Yourself",
+        "srcSong": "./audio/beyonce.mp3"
+
+    },
+    {
+        "src": "./img/dontstartnow.png",
+        "alt": "singer photo",
+        "author": "Dua Lipa",
+        "song": "Don't start now",
+        "srcSong": "./audio/dontstartnow.mp3"
+    },
+    {
+        "src": "./img/linkinPark.jpeg",
+        "alt": "singer photo",
+        "author": "Linkin Park",
+        "song": "Numb",
+        "srcSong": "./audio/Linkin Park - Numb.mp3"
+    },
+    {
+        "src": "./img/tomOdell.jpg",
+        "alt": "singer photo",
+        "author": "Tom Odell",
+        "song": "Another Love",
+        "srcSong": "./audio/Tom Odell-Another Love.mp3"
+    }
+    
+]
